fc452baebc841ed919720ea138f1ff8ae3a4162b
Even Bryhn Bøe <evenboee@gmail.com>
2022-09-02 00:58:40 +0200

    Removed redundant code

--- utils/keyboard.go
+++ utils/keyboard.go
@@ -11,42 +11,17 @@ const KeyRight = uint16(keyboard.KeyArrowRight)
 const KeyDown = uint16(keyboard.KeyArrowDown)
 const KeyLeft = uint16(keyboard.KeyArrowLeft)
 
-type keyboardListener chan KeyEvent
-
 type KeyEvent struct {
 	Char rune
 	Key  uint16
 }
 
-func NewKeyboardListener(channel chan KeyEvent) keyboardListener {
-	return make(keyboardListener)
-}
-
-func (listener keyboardListener) Listen() {
-
-	if err := keyboard.Open(); err != nil {
-		log.Fatalln("Failed to open keyboard")
-	}
-
-	defer func() {
-		err := keyboard.Close()
-		if err != nil {
-			log.Println("failed to close keyboard", err)
-		} else {
-			log.Println("successfully closed keyboard")
-		}
-	}()
-
-	for {
-		char, key, err := keyboard.GetKey()
-		if err != nil {
-			log.Println("failed to get key:", err)
-		}
-
-		listener <- KeyEvent{
-			Char: char,
-			Key:  uint16(key),
-		}
+func CloseKeyboard() {
+	err := keyboard.Close()
+	if err != nil {
+		log.Println("failed to close keyboard", err)
+	} else {
+		log.Println("successfully closed keyboard")
 	}
 }
 
@@ -55,21 +30,14 @@ func KeyBoardListen(handleKeyEvent func(KeyEvent) bool) {
 		log.Fatalln("Failed to open keyboard")
 	}
 
-	defer func() {
-		err := keyboard.Close()
-		if err != nil {
-			log.Println("failed to close keyboard", err)
-		} else {
-			log.Println("successfully closed keyboard")
-		}
-	}()
+	defer CloseKeyboard()
 
 	running := true
 	for running {
-		log.Println("listening", running)
 		char, key, err := keyboard.GetKey()
 		if err != nil {
 			log.Println("failed to get key:", err)
+			return
 		}
 
 		running = !handleKeyEvent(KeyEvent{
